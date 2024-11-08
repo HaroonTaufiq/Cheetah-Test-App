9c0784335ae538b9391427a690455f045555b888
Haroon <itx.haroon1@gmail.com>
2024-11-08 19:04:03 +0500

    server 2.0

--- mongo-server/api/controllers/surveyController.js
+++ mongo-server/api/controllers/surveyController.js
@@ -1,28 +1,36 @@
-
-const { connectToMongo } = require('../db');
+const { connectToMongo } = require("../db");
 
 async function submitSurvey(req, res) {
   let client;
   try {
     client = await connectToMongo();
     const body = req.body;
-    
+
     // Transform the data to the desired format
     const transformedData = {
       email: body.email,
-      first_question: body.data.step1,
-      second_question: JSON.stringify(body.data.step2),
-      submittedAt: new Date()
+      first_question: JSON.stringify(body.data.step2),
+      second_question: JSON.stringify({
+        looks: body.data.step3.looks,
+        price: body.data.step3.price,
+        comfort: body.data.step3.comfort,
+      }),
+      submittedAt: new Date(),
     };
 
-    const database = client.db('survey_db');
-    const surveys = database.collection('surveys');
+    const database = client.db("survey_db");
+    const surveys = database.collection("surveys");
 
     const result = await surveys.insertOne(transformedData);
-    res.status(200).json({ message: 'Survey submitted successfully', id: result.insertedId });
+    res
+      .status(200)
+      .json({
+        message: "Survey submitted successfully",
+        id: result.insertedId,
+      });
   } catch (error) {
-    console.error('Error submitting survey to MongoDB:', error);
-    res.status(500).json({ message: 'Error submitting survey' });
+    console.error("Error submitting survey to MongoDB:", error);
+    res.status(500).json({ message: "Error submitting survey" });
   }
 }
 
@@ -31,14 +39,14 @@ async function checkEmail(req, res) {
   try {
     client = await connectToMongo();
     const { email } = req.body;
-    const database = client.db('survey_db');
-    const surveys = database.collection('surveys');
+    const database = client.db("survey_db");
+    const surveys = database.collection("surveys");
 
     const existingSurvey = await surveys.findOne({ email });
     res.status(200).json({ exists: !!existingSurvey });
   } catch (error) {
-    console.error('Error checking email in MongoDB:', error);
-    res.status(500).json({ message: 'Error checking email' });
+    console.error("Error checking email in MongoDB:", error);
+    res.status(500).json({ message: "Error checking email" });
   }
 }
 
